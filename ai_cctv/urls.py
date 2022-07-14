c1c1a0906b6b8738eff699251eb4f9643ea4ffd1
이현승 <ihyeonseung@ihyeonseung-ui-MacBookPro.local>
2022-07-14 15:06:51 +0900

    make template/paritals

--- ai_cctv/urls.py
+++ ai_cctv/urls.py
@@ -20,5 +20,5 @@ from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('camera/', include('camera.urls')),
-    path('',main,name='main'),
+    path('', main, name='main'),
 ]
