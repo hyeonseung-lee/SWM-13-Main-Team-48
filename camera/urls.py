6cfb6ece6ad3b9ca4fbfca49e603ef35b9461ab9
Ubuntu <ubuntu@ip-172-31-36-91.ap-northeast-2.compute.internal>
2022-07-26 05:14:34 +0000

    배포용

--- camera/urls.py
+++ camera/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from camera.views import *
 
 app_name="camera"
 
 urlpatterns = [
+	path('camerapage', camerapage, name='camerapage'),
+	path('livecam', livecam, name='livecam'),
 
     
 ]
