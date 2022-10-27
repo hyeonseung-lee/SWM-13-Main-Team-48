8810934ac6a3fd8feb09b1498c4c7d9954e5974b
이현승 <ihyeonseung@ihyeonseung-ui-MacBookPro.local>
2022-10-27 18:26:57 +0900

    add navbar

--- camera/views.py
+++ camera/views.py
@@ -16,10 +16,12 @@ from os.path import abspath, join, dirname
 
 
 def camerapage(request):
-    return render(request,'camera.html')
-    
+    return render(request, 'camera.html')
+
+
 @gzip.gzip_page
 def livecam(request):
+    # template으로 넘길 수는 없는지
     try:
         cam = VideoCamera()
         return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
@@ -27,61 +29,66 @@ def livecam(request):
         print("에러입니다...")
         pass
 
+
 @gzip.gzip_page
 def mediapipe(request):
     return StreamingHttpResponse(mediapipe_stream(), content_type="multipart/x-mixed-replace;boundary=frame")
- 
+
+
 @gzip.gzip_page
 def mediapipe_with_yolo(request):
     return StreamingHttpResponse(mediapipe_with_yolo_stream(), content_type="multipart/x-mixed-replace;boundary=frame")
- 
+
+
 @gzip.gzip_page
 def enhancement_yolo(request):
     return StreamingHttpResponse(enhancement_yolo_stream(), content_type="multipart/x-mixed-replace;boundary=frame")
 
+
 def mmaction2(request):
-    return StreamingHttpResponse(mmaction_stream(),content_type="multipart/x-mixed-replace;boundary=frame")
+    return StreamingHttpResponse(mmaction_stream(), content_type="multipart/x-mixed-replace;boundary=frame")
+
 
 def webcam_demo(request):
     # return StreamingHttpResponse([] if webcam_main() is None else webcam_main(),content_type="multipart/x-mixed-replace;boundary=frame")
-    return StreamingHttpResponse(webcam_main(),content_type="multipart/x-mixed-replace;boundary=frame")
-
-
-
+    return StreamingHttpResponse(webcam_main(), content_type="multipart/x-mixed-replace;boundary=frame")
 
 
 def find(request):
     try:
-        date=request.GET['date'].replace('-','')
-        count=Image.objects.filter(image__contains=date).count()
+        date = request.GET['date'].replace('-', '')
+        count = Image.objects.filter(image__contains=date).count()
 
-        return render(request,'find.html',{'date':date,'count':count})
+        return render(request, 'find.html', {'date': date, 'count': count})
     except:
-        return render(request,'find.html')
+        return render(request, 'find.html')
 
-def show(request,date):
-    return render(request,'show.html',{'date':date})
 
-def videostream(request,date):
-    return StreamingHttpResponse(read_and_encode(date),content_type="multipart/x-mixed-replace;boundary=frame")
+def show(request, date):
+    return render(request, 'show.html', {'date': date})
+
+
+def videostream(request, date):
+    return StreamingHttpResponse(read_and_encode(date), content_type="multipart/x-mixed-replace;boundary=frame")
+
 
 def read_and_encode(date):
     # img=cv2.imdecode(image_byte_path,1)
     # print(img)
-    all=Image.objects.filter(image__contains=date)
+    all = Image.objects.filter(image__contains=date)
     for i in all:
-        image=cv2.imread(i.image.path,cv2.IMREAD_COLOR)
+        image = cv2.imread(i.image.path, cv2.IMREAD_COLOR)
         _, jpg = cv2.imencode('.jpg', image)
         frame = jpg.tobytes()
-   
+
         yield(b'--frame\r\n'
-                b'Content-Type: image/jpg\r\n\r\n' + frame + b'\r\n\r\n')
+              b'Content-Type: image/jpg\r\n\r\n' + frame + b'\r\n\r\n')
 
 # @require_POST
 # def po_check(request):
-    
+
 #     poname=json.loads(request.body)
-    
+
 #     poname=poname['po']
 #     # print(poname)
 #     polist={}
@@ -90,10 +97,10 @@ def read_and_encode(date):
 #         list=[]
 #     else:
 #         list=Company.objects.filter(company_name__contains=poname)
-       
+
 #         if list.count()!=1 or (list.count()==1 and list.first().company_name!=poname):
 #             for i in list:
 #                 polist[i.company_code]=i.company_name
-    
+
 #     # print(polist)
 #     return HttpResponse(json.dumps(polist),content_type="application/json")
