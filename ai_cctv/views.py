28f2ceb2cd3039cc93244cd56163c20a1e2a4d72
이현승 <ihyeonseung@ihyeonseung-ui-MacBookPro.local>
2022-11-08 22:13:12 +0900

    add dashboard

--- ai_cctv/views.py
+++ ai_cctv/views.py
@@ -2,14 +2,31 @@ from django.shortcuts import render
 
 
 def main(request):
-    state = False
-    return render(request, 'main.html', )
+    # from DB
+    # from Yolo
+    visitor = 10    # 방문자 수
+    wandor = 2      # 배회
+    # from MMAction2
+    swooner = 4     # 실신
+    vandalism = 2   # 기물파손
+    violence = 3    # 폭행
+
+    # set to front-end
+    # visitor = visitor
+    obstructions = wandor + swooner + violence  # 영업방해행위 (배회 + 실신 + 폭행)
+    # vandalism = vandalism
+    state = {"visitor": visitor,
+             "obstructions": obstructions, "vandalism": vandalism}
+    return render(request, 'main.html', {"state": state})
 
 
 def video_list(request):
     dummy_videos = [
-        {'url': "/######", 'action_type': "실신", 'datetime': "2022-10-29 10:32"},
-        {'url': "/######", 'action_type': "장시간 배회", 'datetime': "2022-10-28 23:33"},
-        {'url': "/######", 'action_type': "기물파손", 'datetime': "2022-10-26 01:22"},
+        {'url': "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+            'action_type': "실신", 'datetime': "2022-10-29 10:32"},
+        {'url': "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+            'action_type': "장시간 배회", 'datetime': "2022-10-28 23:33"},
+        {'url': "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+            'action_type': "기물파손", 'datetime': "2022-10-26 01:22"},
     ]
     return render(request, 'video_list.html', {"dummy_videos": dummy_videos})
--- camera/views.py
+++ camera/views.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from datetime import datetime
 import os
 
+
 def camerapage(request):
     return render(request, 'camera.html')
 
@@ -50,36 +51,40 @@ def enhancement_yolo(request):
 
 
 def mmaction2(request):
-   
-    return StreamingHttpResponse(mmaction_stream(),content_type="multipart/x-mixed-replace;boundary=frame")
+
+    return StreamingHttpResponse(mmaction_stream(), content_type="multipart/x-mixed-replace;boundary=frame")
+
 
 def webcam_demo(request):
-        
-    return StreamingHttpResponse(webcam_main(),content_type="multipart/x-mixed-replace;boundary=frame")
 
+    return StreamingHttpResponse(webcam_main(), content_type="multipart/x-mixed-replace;boundary=frame")
 
 
 # ------------- 아래가 주요 사용 됨 ---------------
 def webcam_thread(request):
-        
-    return StreamingHttpResponse(webcam_thread_main(request),content_type="multipart/x-mixed-replace;boundary=frame")
+
+    return StreamingHttpResponse(webcam_thread_main(request), content_type="multipart/x-mixed-replace;boundary=frame")
 
 
 def find(request):
-   
+
     try:
 
-        start_date=datetime.strptime(request.GET['start'],'%m/%d/%Y')
-        end_date=datetime.strptime(request.GET['end'],'%m/%d/%Y')
-        videos = Video.objects.filter(Q(datetime__lte=end_date)&Q(datetime__gte=start_date)).order_by('-datetime')
+        start_date = datetime.strptime(request.GET['start'], '%m/%d/%Y')
+        end_date = datetime.strptime(request.GET['end'], '%m/%d/%Y')
+        videos = Video.objects.filter(Q(datetime__lte=end_date) & Q(
+            datetime__gte=start_date)).order_by('-datetime')
         # result=[]
         for i in videos:
-            path='/'.join(i.video.split('/')[-4:])
-            path=os.path.join('../../',path)
-            i.video=path
+            path = '/'.join(i.video.split('/')[-4:])
+            path = os.path.join('../../', path)
+            i.video = path
             # result.append(path)
-        return render(request, 'find.html',{"videos":videos})
+        return render(request, 'find.html', {"videos": videos})
 
     except:
         return render(request, 'find.html')
 
+
+def livepage(request):
+    return render(request, 'livepage.html')
