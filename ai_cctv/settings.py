195f49f75f32fa46e590d8f032d15da4e19cca67
이태검 <taegeom0601@gmail.com>
2022-11-23 01:39:07 +0900

    staticfile 경로, root 경로 재세팅

--- ai_cctv/settings.py
+++ ai_cctv/settings.py
@@ -187,10 +187,12 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[ os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"ai_cctv","static")]
+
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
+
 
 
 MEDIA_URL = '/media/'
